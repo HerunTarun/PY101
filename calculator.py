cffc6fd0c5d6d9451bd02b16116d6309d3d93ad1
Herun Tarun <herun.s@gmail.com>
2024-03-23 13:17:54 +0000

    Walkthrough: Calculator complete

--- calculator.py
+++ calculator.py
@@ -1,7 +1,24 @@
-noun = input("Enter a noun: ")
+print('Welcome to Calculator!')
 
-match noun:
-    case "hello":
-        print("hey!")
-    case _:
-        print("Hey, listen!")
+# Ask the user for the first number
+number1 = int(input("What's the first number? "))
+
+# Ask the user for the second number
+number2 = int(input("What's the second number? "))
+
+# Ask the user for an operation to perform
+operation = input("Select a number for the operation you would like\n"
+                "1) Add 2) Subtract 3) Multiply 4) Divide\n")
+
+# Perform the operation on the two numbers
+if operation == '1':
+    output = number1 + number2
+elif operation == '2':
+    output = number1 - number2
+elif operation == '3':
+    output = number1 * number2
+elif operation == '4':
+    output = number1 / number2
+
+# Print the result to the terminal
+print(f'The result is: {output}')
