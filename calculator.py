c554e9bcfa2215b9dbaa5d157623b0ac60d9c565
Herun Tarun <herun.s@gmail.com>
2024-03-25 19:35:49 +0000

    completed calculator bonus feature 1 added feature to ask user for another calculation

--- calculator.py
+++ calculator.py
@@ -8,46 +8,45 @@ def invalid_number(number_str): # checks for invalid operand input
         return True
     return False
 
-prompt('Welcome to Calculator!')
+prompt("Welcome to the Calculator Program!")
 
-# Ask the user for the first number
-prompt("What's the first number? ")
-number1 = input()
-
-# Validate first number
-while invalid_number(number1):
-    prompt("Hmm, that doesn't look like a valid number")
+while True:
+    prompt("What's the first number? ")
     number1 = input()
+    
+    while invalid_number(number1):
+        prompt("Hmm, that doesn't look like a valid number")
+        number1 = input()
 
-# Ask the user for the second number
-prompt("What's the second number? ")
-number2 = input()
-
-# Validate second number
-while invalid_number(number2):
-    prompt("Hmm, that doesn't look like a valid number")
+    prompt("What's the second number? ")
     number2 = input()
 
-# Ask the user for an operation to perform
-prompt("Select a number for the operation you would like to perform\n"
-       "1) Add 2) Subtract 3) Multiply 4) Divide")
-operation = input()
+    while invalid_number(number2):
+        prompt("Hmm, that doesn't look like a valid number")
+        number2 = input()
 
-# Validate operation choice
-while operation not in ['1', '2', '3', '4']:
-    prompt('Please choose 1, 2, 3, or 4!')
+    prompt("Select a number for the operation you would like to perform\n"
+           "1) Add 2) Subtract 3) Multiply 4) Divide")
     operation = input()
 
-# Perform the operation on the two numbers
-match operation:
-    case '1':     # 1 represents addition
-        output = int(number1) + int(number2)
-    case '2':     # 2 represents subtraction
-        output = int(number1) - int(number2)
-    case '3':      # 3 represents multiplication
-        output = int(number1) * int(number2)
-    case '4':      # 4 represents division
-        output = int(number1) / int(number2)
+    while operation not in ['1', '2', '3', '4']:
+        prompt('Please choose 1, 2, 3, or 4!')
+        operation = input()
 
-# Print the result to the terminal
-prompt(f'The result is: {output}')
+    match operation:
+        case '1':     # 1 represents addition
+            output = int(number1) + int(number2)
+        case '2':     # 2 represents subtraction
+            output = int(number1) - int(number2)
+        case '3':      # 3 represents multiplication
+            output = int(number1) * int(number2)
+        case '4':      # 4 represents division
+            output = int(number1) / int(number2)
+
+    prompt(f'The result is: {output}')
+    
+    prompt(f'Do you want to perform more calculations?\n'
+            'Please select "y" or "n".')
+    retry = input()
+    if retry != "y":
+        break
