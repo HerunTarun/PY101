cc902f99299569b3c0f96e87f8d685ab73b6ea5f
Herun Tarun <herun.s@gmail.com>
2024-03-30 16:41:44 +0000

    rps bonus features restructured program to use functions

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -13,63 +13,64 @@ WINNING_COMBINATIONS = {'rock': ['scissors', 'lizard'],
 def prompt(message):
     print(f'==> {message}')
 
+def game_rules():
+    prompt('placeholder')
+    
 def print_welcome():
-    print(messages['welcome'])
-    print(messages['game_rules'])
+    prompt(messages['welcome'])
+    prompt(messages['game_rules'])
 
 def obtain_user_choice():
-    # add input
-    prompt('placeholder')
+    print(f'Choose one: {", ".join(VALID_CHOICES)}')
+    choice = input()
+    while choice not in VALID_CHOICES:
+        print("That's not a valid choice")
+        choice = input()
+
+    return choice
 
 def is_invalid_input():
     # add input validation
     prompt('placeholder')
 
 def generate_computer_choice():
-    prompt('placeholder')
+    computer_choice = random.choice(VALID_CHOICES)
+    
+    return computer_choice
 
-def calculate_winner(choice, computer_choice):
+def calculate_winner():
+    choice = obtain_user_choice()
+    computer_choice = generate_computer_choice()
+    
     if choice == computer_choice:
-        return messages['tie']
+        return messages['tie'].format(choice = choice, 
+                                     computer_choice = computer_choice)
 
     if computer_choice in WINNING_COMBINATIONS[choice]:
-        return messages['win']
+        return messages['win'].format(choice = choice, 
+                                      computer_choice = computer_choice)
         
-    return messages['lose']
+    return messages['lose'].format(choice = choice, 
+                                   computer_choice = computer_choice)
 
-def print_winner(choice, computer_choice):
-    prompt(calculate_winner(choice, computer_choice))
+def print_winner():
+    print(calculate_winner())
 
 def play_again():
-    # add play again function
-    prompt('placeholder')
+    answer = input()
+    while answer.lower() not in ['y', 'yes']:
+        return False
+    os.system('clear')
+    return True
 
 def start_game():
     os.system('clear')
     print_welcome()
     while True:
-        prompt(f'Choose one: {", ".join(VALID_CHOICES)}')
-        choice = input()
-        while choice not in VALID_CHOICES:
-            prompt("That's not a valid choice")
-            choice = input()
-
-        computer_choice = random.choice(VALID_CHOICES)
-
-        prompt(f'You chose {choice}, computer chose {computer_choice}')
-
-        print_winner(choice, computer_choice)
-
+        print_winner()
         prompt("Do you want to play again (y/n)?")
-        answer = input().lower()
-        while True:
-            if answer.startswith('n') or answer.startswith('y'):
-                break
-
-            prompt("Please enter 'y' or 'n'.")
-            answer = input().lower()
-
-        if answer[0] == 'n':
+        if not play_again():
+            print(messages['goodbye'])
             break
 
 with open('rps_messages.json', 'r') as file:
@@ -79,9 +80,9 @@ start_game()
     
 # TODO
 # X restructure program to use functions
-# add json file for messages
-# Add clear screen at start of program and at replay
-# add goodbye message
+# X add json file for messages
+# X Add clear screen at start of program and at replay
+# X add goodbye message
 # add help function
 # add shortened input bonus feature
 # add best of five bonus feature
