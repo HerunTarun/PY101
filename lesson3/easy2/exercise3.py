216097fd84ffa721619bf373b044652e6a87c71f
Herun Tarun <herun.s@gmail.com>
2024-04-05 20:50:54 +0000

    lesson 3 easy 2 problems complete

---
+++ lesson3/easy2/exercise3.py
@@ -0,0 +1,6 @@
+# Programmatically determine whether 42 lies between 10 and 100, inclusive. 
+# Do the same for the values 100 and 101,
+
+print(10 <= 42 <= 100)
+print(10 <= 100 <= 100)
+print(10 <= 101 <= 100)
